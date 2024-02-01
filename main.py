4bd6ba94f9c484e9c00874d61fb4ff2b47151977
Colby <colby.b.cooper@gmail.com>
2024-02-01 17:42:23 -0500

    Add API documentation and terms of service route

--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 Main module for the FastAPI application.
 Sets up the database connection and includes the routers for the API.
 """
+import os
 from contextlib import asynccontextmanager
 from tracemalloc import start
 
 from beanie import init_beanie
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 from api import auth, inventory, oauth
 from database import database
@@ -27,7 +29,38 @@ async def lifespan(_: FastAPI):
     # do something after app shutdown
 
 
-app = FastAPI(lifespan=lifespan)
+desc = """
+This is the API for the SPLYD Frontend. It is ued as the middleware between the frontend and the Lightspeed Retail API.
+"""
+
+app = FastAPI(lifespan=lifespan,
+              title="SPLYD Inventory API",
+              description=desc,
+              version="ALPHA",
+              license_info={
+                  "name": "MIT",
+                  "url": "https://splyd.colbyc.dev/terms"
+              },
+              servers=[
+                  {
+                      "url": "https://splyd.colbyc.dev",
+                      "description": "Production Server"
+                  },
+              ],
+              contact={
+                  "name": "GitHub Issues",
+                  "url": "https://github.com/col-bc/splvd-server/issues/new",
+              },
+              redoc_url="/",
+              docs_url="/docs")
+
+
+@app.get("/terms")
+def render_terms():
+    """Render the terms of service page."""
+    path = os.path.join(os.path.dirname(__file__), "LICENSE")
+    return FileResponse(path)
+
 
 app.include_router(auth.router)
 app.include_router(oauth.router)
